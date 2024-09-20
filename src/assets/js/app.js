251bdeefea07ac27e8564851cc18b61c7e8ec428
KadaverHR <hasslicheratte@yandex.ru>
2024-09-20 16:23:26 +0300

    red

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,7 +3,37 @@
 
 $(document).ready(function () {
 
+  //меню
 
+  let burger = document.querySelector('#hamburger-menu-mob');
+  let menu = document.querySelector('#catalog-drop-mob');
+  let menuLinks = menu.querySelectorAll('.catalog-link');
+  let closeMenu = menu.querySelector('#close-mob')
+
+  burger.addEventListener('click', function () {
+    menu.classList.toggle('active');
+    document.body.classList.toggle('stop-scroll');
+  });
+
+  closeMenu.addEventListener('click', function () {
+    menu.classList.toggle('active');
+    document.body.classList.toggle('stop-scroll');
+  });
+
+  menuLinks.forEach(function (el) {
+    el.addEventListener('click', function () {
+      menu.classList.remove('active');
+      document.body.classList.remove('stop-scroll')
+    })
+  });
+
+
+  let containerWidth = document.querySelector('.container').offsetWidth
+  let windowWidth = window.innerWidth;
+  let a = Math.ceil(containerWidth / 3 * 2 + (windowWidth - containerWidth) / 2);
+  menu.style.width = `${a}px`
+
+  ///
 
 
 
